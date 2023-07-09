c8e4ccb1593ac56d27e4dcb400f6b7dc3a75568b
Max Giga <gigamaximwachau@gmail.com>
2023-07-09 12:54:29 +0300

    refactor

---
+++ src/http.rs
@@ -0,0 +1,146 @@
+use std::{
+    char,
+    collections::HashMap,
+    error::Error,
+    fmt::Display,
+    io::{Read, Write},
+    net::TcpStream,
+    str::FromStr,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Method {
+    Get,
+    Post,
+}
+
+impl Display for Method {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Method::Get => "GET",
+                Method::Post => "POST",
+            }
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct MethodParseError(String);
+impl Display for MethodParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Cannot parse {} as POST method", self.0)
+    }
+}
+impl Error for MethodParseError {}
+impl FromStr for Method {
+    type Err = MethodParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_uppercase().as_str() {
+            "GET" => Ok(Self::Get),
+            "POST" => Ok(Self::Post),
+            a => Err(MethodParseError(a.to_owned())),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Request {
+    pub method: Method,
+    pub endpoint: String,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
+}
+
+impl Request {
+    pub fn read(stream: &TcpStream) -> Self {
+        let mut stream = stream.bytes().map(|x| x.unwrap_or(0));
+
+        let line = read_line(&mut stream);
+        let first: Vec<&str> = line.splitn(3, ' ').collect();
+        let mut headers = HashMap::new();
+
+        loop {
+            let line = read_line(&mut stream);
+            if line.trim().is_empty() {
+                break;
+            }
+            let (name, value) = line.split_once(':').unwrap();
+            headers.insert(name.trim().to_owned(), value.trim().to_owned());
+        }
+
+        let body = headers
+            .get("Content-Length")
+            .map(|x| x.parse::<usize>().unwrap_or(0))
+            .and_then(|l| {
+                if l == 0 {
+                    return None;
+                }
+                Some(String::from_utf8_lossy(&stream.take(l).collect::<Vec<u8>>()).to_string())
+            });
+
+        Request {
+            method: first[0].parse().unwrap(),
+            endpoint: first[1].trim().to_owned(),
+            headers,
+            body,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Response {
+    status: usize,
+    headers: HashMap<String, String>,
+    body: String,
+}
+
+impl Response {
+    pub fn new<S: Into<String>>(status: usize, body: S) -> Self {
+        Self {
+            status,
+            headers: HashMap::new(),
+            body: body.into(),
+        }
+    }
+    pub fn with_header<S0: Into<String>, S1: Into<String>>(self, key: S0, val: S1) -> Self {
+        let mut headers = self.headers;
+        headers.insert(key.into(), val.into());
+        Self {
+            status: self.status,
+            headers,
+            body: self.body,
+        }
+    }
+    pub fn write(&self, s: &mut TcpStream) -> Result<(), std::io::Error> {
+        s.write_all(self.to_string().as_bytes())?;
+        s.flush()
+    }
+}
+impl Display for Response {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "HTTP/1.1 {}\n{}\n{}",
+            self.status,
+            self.headers
+                .iter()
+                .map(|(key, value)| format!("{key}: {value}\n"))
+                .collect::<String>(),
+            self.body
+        )
+    }
+}
+
+fn read_line<I: Iterator<Item = u8>>(i: &mut I) -> String {
+    let mut s = String::new();
+    for c in i {
+        if c == b'\n' {
+            return s;
+        }
+        s.push(char::from_u32(c as u32).unwrap());
+    }
+    s
+}
--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,13 @@
 use std::{
-    char,
     collections::HashMap,
     error::Error,
-    fmt::Display,
-    io::{Read, Write},
     net::{TcpListener, TcpStream},
-    str::FromStr,
     sync::{Arc, OnceLock},
 };
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum Method {
-    Get,
-    Post,
-}
+use http::{Method, Request, Response};
 
-impl Display for Method {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Method::Get => "GET",
-                Method::Post => "POST",
-            }
-        )
-    }
-}
-
-#[derive(Debug)]
-struct MethodParseError(String);
-impl Display for MethodParseError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Cannot parse {} as POST method", self.0)
-    }
-}
-impl Error for MethodParseError {}
-impl FromStr for Method {
-    type Err = MethodParseError;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_uppercase().as_str() {
-            "GET" => Ok(Self::Get),
-            "POST" => Ok(Self::Post),
-            a => Err(MethodParseError(a.to_owned())),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Request {
-    method: Method,
-    endpoint: String,
-    headers: HashMap<String, String>,
-    body: Option<String>,
-}
-
-impl Request {
-    pub fn read(stream: &TcpStream) -> Self {
-        let mut stream = stream.bytes().map(|x| x.unwrap_or(0));
-
-        let line = read_line(&mut stream);
-        let first: Vec<&str> = line.splitn(3, ' ').collect();
-        let mut headers = HashMap::new();
-
-        loop {
-            let line = read_line(&mut stream);
-            if line.trim().is_empty() {
-                break;
-            }
-            let (name, value) = line.split_once(':').unwrap();
-            headers.insert(name.trim().to_owned(), value.trim().to_owned());
-        }
-
-        let body = headers
-            .get("Content-Length")
-            .map(|x| x.parse::<usize>().unwrap_or(0))
-            .and_then(|l| {
-                if l == 0 {
-                    return None;
-                }
-                Some(String::from_utf8_lossy(&stream.take(l).collect::<Vec<u8>>()).to_string())
-            });
-
-        Request {
-            method: first[0].parse().unwrap(),
-            endpoint: first[1].trim().to_owned(),
-            headers,
-            body,
-        }
-    }
-}
-
-struct Response {
-    status: usize,
-    headers: HashMap<String, String>,
-    body: String,
-}
-
-impl Response {
-    pub fn new<S: Into<String>>(status: usize, body: S) -> Self {
-        Self {
-            status,
-            headers: HashMap::new(),
-            body: body.into(),
-        }
-    }
-    pub fn with_header<S0: Into<String>, S1: Into<String>>(self, key: S0, val: S1) -> Self {
-        let mut headers = self.headers;
-        headers.insert(key.into(), val.into());
-        Self {
-            status: self.status,
-            headers,
-            body: self.body,
-        }
-    }
-    pub fn write(&self, s: &mut TcpStream) -> Result<(), std::io::Error> {
-        s.write_all(self.to_string().as_bytes())?;
-        s.flush()
-    }
-}
-impl Display for Response {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "HTTP/1.1 {}\n{}\n{}",
-            self.status,
-            self.headers
-                .iter()
-                .map(|(key, value)| format!("{key}: {value}\n"))
-                .collect::<String>(),
-            self.body
-        )
-    }
-}
+mod http;
 
 type HandlerKey = (Method, String);
 type Handler = Arc<dyn Fn(Request) -> Response + Send + Sync>;
@@ -162,47 +37,36 @@ fn main() -> Result<(), Box<dyn Error>> {
         .with_header("Zggff", "12")
     });
     set_handler!(handlers, Method::Get, "/", |_request: Request| {
-        Response::new(
-            200,
-            format!(
-                "Hello There",
-            ),
-        )
+        Response::new(200, format!("Hello There",))
     });
 
     if HANDLERS.set(handlers).is_err() {
         panic!("failed to initialise handlers")
     }
 
-    for stream in listener.incoming() {
-        let mut stream = stream?;
-        let request = Request::read(&stream);
-        if let Some(handler) = HANDLERS
-            .get()
-            .and_then(|h| h.get(&(request.method, request.endpoint.clone())))
-        {
-            let response = handler(request);
-            response.write(&mut stream)?;
-        } else if let Some(handler) = HANDLERS
-            .get()
-            .and_then(|h| h.get(&(request.method, "*".to_string()))) {
-            let response = handler(request);
-            response.write(&mut stream)?;
-        } else {
-            let response = Response::new(404, "Not Found".to_string());
-            response.write(&mut stream)?;
-        }
+    for stream in listener.incoming().flatten() {
+        handle_stream(stream)?;
     }
     Ok(())
 }
 
-fn read_line<I: Iterator<Item = u8>>(i: &mut I) -> String {
-    let mut s = String::new();
-    for c in i {
-        if c == b'\n' {
-            return s;
-        }
-        s.push(char::from_u32(c as u32).unwrap());
+fn handle_stream(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let request = Request::read(&stream);
+    if let Some(handler) = HANDLERS
+        .get()
+        .and_then(|h| h.get(&(request.method, request.endpoint.clone())))
+    {
+        let response = handler(request);
+        response.write(&mut stream)?;
+    } else if let Some(handler) = HANDLERS
+        .get()
+        .and_then(|h| h.get(&(request.method, "*".to_string())))
+    {
+        let response = handler(request);
+        response.write(&mut stream)?;
+    } else {
+        let response = Response::new(404, "Not Found".to_string());
+        response.write(&mut stream)?;
     }
-    s
+    Ok(())
 }
